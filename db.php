cb63aadb18376cb05e2d92f7c2967df60d036d08
Lowseyboy <99146511+LN4DP4@users.noreply.github.com>
2022-03-25 08:33:54 +0000

    ywa

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$dbname = "gibjohn";
+
+$connection = mysqli_connect($hostname, $username, $password, $dbname,) or die("Database connection not established.");
+
+
+?>
---
+++ user.php
@@ -0,0 +1,65 @@
+<?php
+
+class user {
+    public $email = "";
+    public $password = "";
+    public $password_hash = "";
+    public $authenticated = false;
+    private $connection;
+
+    function __construct($connection, $first_name, $email, $password) {
+        $this->first_name = mysqli_real_escape_string($connection, $first_name);
+        $this->email = mysqli_real_escape_string($connection, $email);
+        $this->password = mysqli_real_escape_string($connection, $password);
+
+        $this->password_hash = password_hash($password, PASSWORD_BCRYPT);
+
+        $this->connection = $connection;
+
+    }
+
+    function insert() {
+        $sql = "
+        Insert INTO student(
+            first_name,
+            email,
+            password,
+            status
+        )
+        VALUES (
+            '{$this->first_name}',
+            '{$this->email}',
+            '{$this->password_hash}',
+            'active'
+        )    
+        ";
+        
+        $sqlQuery = mysqli_query($this->connection, $sql);
+
+        if(!$sqlQuery){
+
+            die("MySQL query failed!" . mysqli_error($this->connection));
+        }
+
+    }
+
+    function authenticate() {
+        $sql = "
+            SELECT student_id, email, password, status
+            FROM student
+            WHERE email=\"{$this->email}\";
+            ";
+        
+            $result = $this->connection->query($sql);
+            if ($row = $result->fetch_assoc()) {
+
+                if(password_verify($this->password, $row['password'])) {
+                    $this->authenticated = true;
+                }
+            }
+    }
+
+    function is_logged_in(){
+        return $this->authenticated;
+    }
+}
